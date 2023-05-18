619a342670fe9145637d13d9a4a89b57b9e28a12
Lakshmi Narasimhan S <bachelorofphotography@gmail.com>
2023-05-18 08:03:35 +0530

    Index update
    add some new concepts

--- test.js
+++ test.js
@@ -1,22 +1,19 @@
-function bubbleSort(arr) {
-  const len = arr.length;
+function bubbleSortFunction(arr) {
+  const arrayLength = arr.length;
   let swapped = false;
 
-  do {
-    swapped = false;
-    for (let i = 0; i < len - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        const temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-        swapped = true;
-      }
+  for (let i = 0; i < arrayLength - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      const tempArr = arr[i];
+      arr[i] = arr[i + 1];
+      arr[i + 1] = tempArr;
+      swapped = true;
     }
-  } while (swapped);
+  }
+  while (swapped);
 
   return arr;
 }
 
-// Example usage:
-const arr = [5, 3, 8, 4, 2];
-console.log(bubbleSort(arr)); // Output: [2, 3, 4, 5, 8]
+const arr = [4, 7, 1, 3];
+console.log();
